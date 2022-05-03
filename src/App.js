04f3c47ccf92ade9cea9298e316951fd25aaeca2
ShinyDuck21 <nlowinger3@gmail.com>
2022-05-03 16:54:21 -0500

    main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import { signOut } from 'firebase/auth';
-import GoogleButton from 'react-google-button';
+import logo from './logo.svg';
 import './App.css';
-import { signInWithGoogle, SignOut } from "./firebase"
 
 function App() {
   return (
-    <>
-      <GoogleButton onClick={signInWithGoogle} />
-      <h1>Logged in as {localStorage.getItem("name")}</h1>
-      <button onClick={SignOut}>Log Out</button>
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
 export default App;
--- src/PersonPage.js
+++
@@ -1,7 +0,0 @@
-function UserPage() {
-    return (
-    <>
-        <h1>User TEMP</h1>
-    </>
-    )
-}
